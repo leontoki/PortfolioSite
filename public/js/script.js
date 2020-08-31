69f37ea985580a6829fda7d41f9962477a1b782f
leontoki <64117921+leontoki@users.noreply.github.com>
2020-08-31 16:15:17 +0900

    commit20200831-01
    JQueryでレスポンシブデザインの
    ハンバーガーメニュー実装

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+$(function () {
+
+  // $('#hosoi-picture').fadeIn(800);
+
+  $('#open-btn').click(function () {
+    $('#ham-menu').slideDown(100);
+  });
+
+  $('#close-btn').click(function () {
+    $('#ham-menu').slideUp(100);
+  });
+
+
+
+
+});
